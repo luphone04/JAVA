37ad830eac8aa0c9aba29de75b27a77f92573498
luphone04 <luphonemaw09@gmail.com>
2023-01-12 13:26:37 +0700

    Inheritance

--- untitled/src/Inheritance/Bicycle.java
+++ untitled/src/Inheritance/Bicycle.java
@@ -1,5 +1,14 @@
 package Inheritance;
 
+
+//To share similar properties and similar attributes
+//There would be many kind of similarity across all of those types of CLASSES but there will also be many kinds of difference (common behavior and different behavior )
+//Remember the phrase “is a type of …. ”
+//The child type class will be inheriting the properties from base parent class
+//The child class is expanding from the parent class. And also those children can expand all the other children under them
+//Initialize the base class constructor by calling the super method in the child class constructor
+//Example super(startSpeed , startGear);
+
 class Bicycle {
     protected int gear;
     protected int speed;
