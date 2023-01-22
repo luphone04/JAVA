646323eadffb4cb78308d4f2cdcf5232884992ae
luphone04 <luphonemaw09@gmail.com>
2023-01-22 18:15:35 +0700

    if else statement

--- untitled/src/practiceMyself/Main.java
+++ untitled/src/practiceMyself/Main.java
@@ -2,28 +2,37 @@ package practiceMyself;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in); // Do not change this line // Write your program here
+        Scanner scan = new Scanner(System.in);
 
-         System.out.print("What is your name? ");
-         String name = scanner.nextLine(); // read user input
+        System.out.print("Enter the first number: ");
+        double number1 = scan.nextDouble();
+        scan.nextLine();
 
+        System.out.print("Enter the second number: ");
+        double number2 = scan.nextDouble();
+        scan.nextLine();
 
+        System.out.print("What operation do you wnat to perform?");
+        String operation = scan.nextLine();
 
-        System.out.printf("Hello %s How old are you? ", name);
-        int age = scanner.nextInt();
+        if (operation.equals("+")) {
+            System.out.printf("%f + %f = %f", number1, number2, number1 + number2);
+        } else if (operation.equals("-")) {
+            System.out.printf("%f - %f = %f", number1, number2, number1 - number2);
 
-//        double gpa = Double.parseDouble(scanner.nextLine()); // read user input
+        } else if (operation.equals("*")) {
+            System.out.printf("%f * %f = %f", number1, number2, number1 * number2);
+        } else if (operation.equals("/")) {
+            if(number2 == 0){
+                System.out.println("You cannot divide by zero");
+            }else{
+                System.out.printf("%f / %f = %f", number1, number2, number1 / number2);
+            }
 
+        }else{
+            System.out.print("Invalid Operation");
+        }
 
-        scanner.nextLine(); // read the newline character
-
-        System.out.printf("%d is an excellent age to start programming! WHat language do you prefer?", age);
-        String language = scanner.nextLine();
-
-        System.out.printf("%s is a very populart programming language.", language);
-
-
-        scanner.close(); // close the scanner
-
+        scan.close();
     }
 }
