774b70773cacb558ed25af394fddadb8cae2f96f
luphone04 <luphonemaw09@gmail.com>
2023-01-22 18:26:01 +0700

    Switch cases

--- untitled/src/practiceMyself/Main.java
+++ untitled/src/practiceMyself/Main.java
@@ -15,23 +15,45 @@ public class Main {
         System.out.print("What operation do you wnat to perform?");
         String operation = scan.nextLine();
 
-        if (operation.equals("+")) {
-            System.out.printf("%f + %f = %f", number1, number2, number1 + number2);
-        } else if (operation.equals("-")) {
-            System.out.printf("%f - %f = %f", number1, number2, number1 - number2);
-
-        } else if (operation.equals("*")) {
-            System.out.printf("%f * %f = %f", number1, number2, number1 * number2);
-        } else if (operation.equals("/")) {
-            if(number2 == 0){
-                System.out.println("You cannot divide by zero");
-            }else{
-                System.out.printf("%f / %f = %f", number1, number2, number1 / number2);
-            }
-
-        }else{
-            System.out.print("Invalid Operation");
+        switch(operation){
+            case "+":
+                System.out.println("number1 + number2 = " + (number1 + number2));
+                break;
+            case "-":
+                System.out.println("number1 - number2 = " + (number1 - number2));
+                break;
+            case "*":
+                System.out.println("number1 * number2 = " + (number1 * number2));
+                break;
+            case "/":
+                if(number2 == 0){
+                    System.out.println("Cannot divide by zero");
+                }else{
+                    System.out.println("number1 / number2 = " + (number1 / number2));
+                }
+                break;
+            default:
+                System.out.println("Invalid operation"); // This will also print out this even if the condition is true if you don't break;
         }
+
+
+//        if (operation.equals("+")) {
+//            System.out.printf("%f + %f = %f", number1, number2, number1 + number2);
+//        } else if (operation.equals("-")) {
+//            System.out.printf("%f - %f = %f", number1, number2, number1 - number2);
+//
+//        } else if (operation.equals("*")) {
+//            System.out.printf("%f * %f = %f", number1, number2, number1 * number2);
+//        } else if (operation.equals("/")) {
+//            if(number2 == 0){
+//                System.out.println("You cannot divide by zero");
+//            }else{
+//                System.out.printf("%f / %f = %f", number1, number2, number1 / number2);
+//            }
+//
+//        }else{
+//            System.out.print("Invalid Operation");
+//        }
 
         scan.close();
     }
