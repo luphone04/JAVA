38cc857ae9fc03d26918272b9ec2fd97be3ffedc
luphone04 <luphonemaw09@gmail.com>
2023-02-07 11:32:18 +0700

    Nest Inversion Extraction

--- untitled/src/test_package/Main.java
+++ untitled/src/test_package/Main.java
@@ -4,8 +4,33 @@ import oocpyoutube.accessmodifier;
 
 public class Main {
 
-        public static void main(String[] args) {
-           accessmodifier t = new accessmodifier();
-           t.display();
+    static int checkEven(int num){
+        if(num % 2 == 0){
+            return num;
         }
+        return 0;
+    }
+
+    //If we have a lot of condition to check , we can apply inversion over and over again.
+    static int caculate(int bottom, int top){
+        if(top < bottom){
+            return 0; //since we can return here, we don't need to use else so bye bye the else
+        }
+
+            int sum = 0;
+            for(int num = bottom ; num <= top ; num++){
+                sum += checkEven(num);
+            }
+            return sum;
+
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(caculate(1, 10));
+
+
+    }
+
 }
